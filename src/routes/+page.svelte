97c51a5ec0a30e84b09b4ce287e7e54c7e9866c8
Ruán <ruan@murgatroyd.me>
2023-10-03 13:43:21 +0100

    Basic fill in / route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,17 @@
     <Row>
         <Col>
             <h1 class="title large">ByteStore</h1>
+            <h2 class="subtitle medium nogap">A Society Inventory System</h2>
+        </Col>
+    </Row>
+
+    <Row>
+        <Col>
+            <div class="exterior">
+                <h1 class="title medium">Log In</h1>
+
+                Replace with SSO login buttons
+            </div>
         </Col>
     </Row>
 </Container>
